83ae944b3c2809d1334f1c2c9c45abc9c748108f
lunnasouzaa <souzalunn4@gmail.com>
2026-08-31 19:55:38 -0300

    CRUD de disciplinas

--- disciplinas/urls.py
+++ disciplinas/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+
 from . import views
+
 
 urlpatterns = [
     path('', views.listar_disciplinas, name='listar_disciplinas'),
     path('nova/', views.criar_disciplina, name='criar_disciplina'),
+    path('<int:id>/', views.detalhar_disciplina, name='detalhar_disciplina'),
+    path('<int:id>/editar/', views.editar_disciplina, name='editar_disciplina'),
+    path('<int:id>/excluir/', views.excluir_disciplina, name='excluir_disciplina'),
 ]
--- disciplinas/views.py
+++ disciplinas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Disciplina
 from .forms import DisciplinaForm
 
@@ -27,4 +27,50 @@ def criar_disciplina(request):
         request,
         'disciplinas/criar.html',
         {'form': form}
+    )
+
+
+def detalhar_disciplina(request, id):
+    disciplina = get_object_or_404(Disciplina, id=id)
+
+    return render(
+        request,
+        'disciplinas/detalhar.html',
+        {'disciplina': disciplina}
+    )
+
+
+def editar_disciplina(request, id):
+    disciplina = get_object_or_404(Disciplina, id=id)
+
+    if request.method == 'POST':
+        form = DisciplinaForm(request.POST, instance=disciplina)
+
+        if form.is_valid():
+            form.save()
+            return redirect('detalhar_disciplina', id=disciplina.id)
+    else:
+        form = DisciplinaForm(instance=disciplina)
+
+    return render(
+        request,
+        'disciplinas/editar.html',
+        {
+            'form': form,
+            'disciplina': disciplina
+        }
+    )
+
+
+def excluir_disciplina(request, id):
+    disciplina = get_object_or_404(Disciplina, id=id)
+
+    if request.method == 'POST':
+        disciplina.delete()
+        return redirect('listar_disciplinas')
+
+    return render(
+        request,
+        'disciplinas/excluir.html',
+        {'disciplina': disciplina}
     )
