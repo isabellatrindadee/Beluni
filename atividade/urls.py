ea7ead185c9bd85a1382baaf0e4e777822236fe7
N1c0ll1 <nicollysalves714@gmail.com>
2026-08-31 21:14:57 -0300

    CRUD completo de atividade

--- atividade/urls.py
+++ atividade/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.listar_atividades, name='listar_atividades'),
     path('nova/', views.criar_atividade, name='criar_atividade'),
+    path('<int:id>/', views.detalhar_atividade, name='detalhar_atividade'),
+    path('<int:id>/editar/', views.editar_atividade, name='editar_atividade'),
+    path('<int:id>/excluir/', views.excluir_atividade, name='excluir_atividade'),
 ]
--- atividade/views.py
+++ atividade/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Atividade
 from .forms import AtividadeForm
 
@@ -27,4 +27,50 @@ def criar_atividade(request):
         request,
         'atividade/criar.html',
         {'form': form}
+    )
+
+
+def detalhar_atividade(request, id):
+    atividade = get_object_or_404(Atividade, id=id)
+
+    return render(
+        request,
+        'atividade/detalhar.html',
+        {'atividade': atividade}
+    )
+
+
+def editar_atividade(request, id):
+    atividade = get_object_or_404(Atividade, id=id)
+
+    if request.method == 'POST':
+        form = AtividadeForm(request.POST, instance=atividade)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_atividades')
+    else:
+        form = AtividadeForm(instance=atividade)
+
+    return render(
+        request,
+        'atividade/editar.html',
+        {
+            'form': form,
+            'atividade': atividade
+        }
+    )
+
+
+def excluir_atividade(request, id):
+    atividade = get_object_or_404(Atividade, id=id)
+
+    if request.method == 'POST':
+        atividade.delete()
+        return redirect('listar_atividades')
+
+    return render(
+        request,
+        'atividade/excluir.html',
+        {'atividade': atividade}
     )
