c9b8d89a281bbc3dbc745e8b9801827d54e0b5d8
N1c0ll1 <nicollysalves714@gmail.com>
2026-08-31 21:26:58 -0300

    CRUD completo de meta

--- meta/urls.py
+++ meta/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.listar_metas, name='listar_metas'),
     path('nova/', views.criar_meta, name='criar_meta'),
+    path('<int:id>/', views.detalhar_meta, name='detalhar_meta'),
+    path('<int:id>/editar/', views.editar_meta, name='editar_meta'),
+    path('<int:id>/excluir/', views.excluir_meta, name='excluir_meta'),
 ]
--- meta/views.py
+++ meta/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Meta
 from .forms import MetaForm
 
@@ -27,4 +27,50 @@ def criar_meta(request):
         request,
         'meta/criar.html',
         {'form': form}
+    )
+
+
+def detalhar_meta(request, id):
+    meta = get_object_or_404(Meta, id=id)
+
+    return render(
+        request,
+        'meta/detalhar.html',
+        {'meta': meta}
+    )
+
+
+def editar_meta(request, id):
+    meta = get_object_or_404(Meta, id=id)
+
+    if request.method == 'POST':
+        form = MetaForm(request.POST, instance=meta)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_metas')
+    else:
+        form = MetaForm(instance=meta)
+
+    return render(
+        request,
+        'meta/editar.html',
+        {
+            'form': form,
+            'meta': meta
+        }
+    )
+
+
+def excluir_meta(request, id):
+    meta = get_object_or_404(Meta, id=id)
+
+    if request.method == 'POST':
+        meta.delete()
+        return redirect('listar_metas')
+
+    return render(
+        request,
+        'meta/excluir.html',
+        {'meta': meta}
     )
